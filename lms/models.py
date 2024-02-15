a7c811b1bb85886f39de78ece380ed054de250c1
PyDevJ <gitcloudhab@gmail.com>
2024-02-16 00:05:53 +0300

    create models: User, Course, Lesson

--- lms/models.py
+++ lms/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+from users.models import NULLABLE
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=100, verbose_name='название')
+    preview = models.ImageField(upload_to='course/', verbose_name='превью', **NULLABLE)
+    description = models.TextField(verbose_name='описание', **NULLABLE)
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'курс'
+        verbose_name_plural = 'курсы'
+
+
+class Lesson(models.Model):
+    name = models.CharField(max_length=100, verbose_name='название')
+    description = models.TextField(verbose_name='описание', **NULLABLE)
+    preview = models.ImageField(upload_to='lesson/', verbose_name='превью', **NULLABLE)
+    video_link = models.URLField(verbose_name='ссылка на видео', **NULLABLE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, verbose_name='курс', **NULLABLE)
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'урок'
+        verbose_name_plural = 'уроки'
--- users/models.py
+++ users/models.py
@@ -1,5 +1,22 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+NULLABLE = {'null': True, 'blank': True}
 
 
 class User(AbstractUser):
-    pass
+    username = None
+    email = models.EmailField(unique=True, verbose_name='email')
+    phone = models.CharField(max_length=20, verbose_name='телефон', **NULLABLE)
+    city = models.CharField(max_length=50, verbose_name='город', **NULLABLE)
+    avatar = models.ImageField(upload_to='users/', verbose_name='аватарка', **NULLABLE)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return f'{self.email}'
+
+    class Meta:
+        verbose_name = 'пользователь'
+        verbose_name_plural = 'пользователи'

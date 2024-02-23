e90367fd7e3c3735494e91cf63e428594004e4b5
PyDevJ <gitcloudhab@gmail.com>
2024-02-24 00:18:27 +0300

    задание 1

--- lms/serializers.py
+++ lms/serializers.py
@@ -3,10 +3,20 @@ from lms.models import Course, Lesson
 
 
 class CourseSerializer(serializers.ModelSerializer):
+    count_lessons = serializers.SerializerMethodField()
 
     class Meta:
         model = Course
-        fields = '__all__'
+        # fields = '__all__'
+        fields = [
+            'id',
+            'name',
+            'description',
+            'count_lessons'
+        ]
+
+    def get_count_lessons(self, instance):
+        return instance.lesson_set.count()
 
 
 class LessonSerializer(serializers.ModelSerializer):
